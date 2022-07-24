73095d6e0b8676a037867fda42ebcecac2e366ee
Dominick Baier <dbaier@leastprivilege.com>
2022-07-24 18:21:14 +0200

    ..and more

---
+++ test/Tests/Framework/AppHost.cs
@@ -0,0 +1,142 @@
+﻿// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net;
+using Microsoft.AspNetCore.Authentication;
+
+namespace Duende.AccessTokenManagement.Tests;
+
+public class AppHost : GenericHost
+{
+    private readonly IdentityServerHost _identityServerHost;
+    private readonly ApiHost _apiHost;
+    private readonly string _clientId;
+        
+    public AppHost(IdentityServerHost identityServerHost, ApiHost apiHost, string clientId,
+        string baseAddress = "https://app")
+        : base(baseAddress)
+    {
+        _identityServerHost = identityServerHost;
+        _apiHost = apiHost;
+        _clientId = clientId;
+
+        OnConfigureServices += ConfigureServices;
+        OnConfigure += Configure;
+    }
+
+    private void ConfigureServices(IServiceCollection services)
+    {
+        services.AddRouting();
+        services.AddAuthorization();
+
+        services.AddAuthentication("cookie")
+            .AddCookie("cookie", options =>
+            {
+                options.Cookie.Name = "bff";
+            });
+
+        services.AddAuthentication(options =>
+            {
+                options.DefaultChallengeScheme = "oidc";
+                options.DefaultSignOutScheme = "oidc";
+            })
+            .AddOpenIdConnect("oidc", options =>
+            {
+                options.Authority = _identityServerHost.Url();
+
+                options.ClientId = _clientId;
+                options.ClientSecret = "secret";
+                options.ResponseType = "code";
+                options.ResponseMode = "query";
+
+                options.MapInboundClaims = false;
+                options.GetClaimsFromUserInfoEndpoint = true;
+                options.SaveTokens = true;
+
+                options.Scope.Clear();
+                var client = _identityServerHost.Clients.Single(x => x.ClientId == _clientId);
+                foreach (var scope in client.AllowedScopes)
+                {
+                    options.Scope.Add(scope);
+                }
+
+                if (client.AllowOfflineAccess)
+                {
+                    options.Scope.Add("offline_access");
+                }
+
+                options.BackchannelHttpHandler = _identityServerHost.Server.CreateHandler();
+            });
+    }
+
+    private void Configure(IApplicationBuilder app)
+    {
+        app.UseAuthentication();
+        app.UseRouting();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapGet("/login", async context =>
+            {
+                await context.ChallengeAsync(new AuthenticationProperties
+                {
+                    RedirectUri = "/"
+                });
+            });
+                
+            endpoints.MapGet("/logout", async context =>
+            {
+                await context.SignOutAsync();
+            });
+        });
+    }
+
+    public async Task<HttpResponseMessage> LoginAsync(string sub, string sid = null)
+    {
+        await _identityServerHost.CreateIdentityServerSessionCookieAsync(sub, sid);
+        return await OidcLoginAsync();
+    }
+
+    public async Task<HttpResponseMessage> OidcLoginAsync()
+    {
+        var response = await BrowserClient.GetAsync(Url("/login"));
+        response.StatusCode.ShouldBe((HttpStatusCode)302); // authorize
+        response.Headers.Location.ToString().ToLowerInvariant().ShouldStartWith(_identityServerHost.Url("/connect/authorize"));
+
+        response = await _identityServerHost.BrowserClient.GetAsync(response.Headers.Location.ToString());
+        response.StatusCode.ShouldBe((HttpStatusCode)302); // client callback
+        response.Headers.Location.ToString().ToLowerInvariant().ShouldStartWith(Url("/signin-oidc"));
+
+        response = await BrowserClient.GetAsync(response.Headers.Location.ToString());
+        response.StatusCode.ShouldBe((HttpStatusCode)302); // root
+        response.Headers.Location.ToString().ToLowerInvariant().ShouldBe("/");
+
+        response = await BrowserClient.GetAsync(Url(response.Headers.Location.ToString()));
+        return response;
+    }
+
+    public async Task<HttpResponseMessage> BffLogoutAsync(string sid = null)
+    {
+        var response = await BrowserClient.GetAsync(Url("/logout") + "?sid=" + sid);
+        response.StatusCode.ShouldBe((HttpStatusCode)302); // endsession
+        response.Headers.Location.ToString().ToLowerInvariant().ShouldStartWith(_identityServerHost.Url("/connect/endsession"));
+
+        response = await _identityServerHost.BrowserClient.GetAsync(response.Headers.Location.ToString());
+        response.StatusCode.ShouldBe((HttpStatusCode)302); // logout
+        response.Headers.Location.ToString().ToLowerInvariant().ShouldStartWith(_identityServerHost.Url("/account/logout"));
+
+        response = await _identityServerHost.BrowserClient.GetAsync(response.Headers.Location.ToString());
+        response.StatusCode.ShouldBe((HttpStatusCode)302); // post logout redirect uri
+        response.Headers.Location.ToString().ToLowerInvariant().ShouldStartWith(Url("/signout-callback-oidc"));
+
+        response = await BrowserClient.GetAsync(response.Headers.Location.ToString());
+        response.StatusCode.ShouldBe((HttpStatusCode)302); // root
+        response.Headers.Location.ToString().ToLowerInvariant().ShouldBe("/");
+
+        response = await BrowserClient.GetAsync(Url(response.Headers.Location.ToString()));
+        return response;
+    }
+}
---
+++ test/Tests/Framework/IntegrationTestBase.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
+using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Security.Claims;
+
+namespace Duende.AccessTokenManagement.Tests;
+
+public class IntegrationTestBase
+{
+    protected readonly IdentityServerHost IdentityServerHost;
+    protected ApiHost ApiHost;
+    protected AppHost AppHost;
+
+    public IntegrationTestBase()
+    {
+        IdentityServerHost = new IdentityServerHost();
+            
+        IdentityServerHost.Clients.Add(new Client
+        {
+            ClientId = "spa",
+            ClientSecrets = { new Secret("secret".Sha256()) },
+            AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+            RedirectUris = { "https://app/signin-oidc" },
+            PostLogoutRedirectUris = { "https://app/signout-callback-oidc" },
+            BackChannelLogoutUri = "https://app/bff/backchannel",
+            AllowOfflineAccess = true,
+            AllowedScopes = { "openid", "profile", "scope1" }
+        });
+            
+            
+        IdentityServerHost.OnConfigureServices += services => {
+            services.AddTransient<IBackChannelLogoutHttpClient>(provider => 
+                new DefaultBackChannelLogoutHttpClient(
+                    AppHost.HttpClient, 
+                    provider.GetRequiredService<ILoggerFactory>(), 
+                    provider.GetRequiredService<ICancellationTokenProvider>()));
+        };
+            
+        IdentityServerHost.InitializeAsync().Wait();
+
+        ApiHost = new ApiHost(IdentityServerHost, "scope1");
+        ApiHost.InitializeAsync().Wait();
+
+        AppHost = new AppHost(IdentityServerHost, ApiHost, "spa");
+        AppHost.InitializeAsync().Wait();
+    }
+
+    public async Task Login(string sub)
+    {
+        await IdentityServerHost.IssueSessionCookieAsync(new Claim("sub", sub));
+    }
+}
