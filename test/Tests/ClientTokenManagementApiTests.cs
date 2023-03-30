f03904cae0178bc325c2025ef68c42f0be45a028
Brock Allen <brockallen@gmail.com>
2023-03-30 08:13:41 -0400

    add simple integration test to exercise some of the dpop client code

--- test/Tests/ClientTokenManagementApiTests.cs
+++ test/Tests/ClientTokenManagementApiTests.cs
@@ -8,15 +8,21 @@ namespace Duende.AccessTokenManagement.Tests;
 
 public class ClientTokenManagementApiTests : IntegrationTestBase
 {
+    private static readonly string _jwkJson;
+
     private HttpClient _client;
     private IClientCredentialsTokenManagementService _tokenService;
 
-    public ClientTokenManagementApiTests()
+    static ClientTokenManagementApiTests()
     {
         var key = CryptoHelper.CreateRsaSecurityKey();
         var jwk = JsonWebKeyConverter.ConvertFromRSASecurityKey(key);
         jwk.Alg = "RS256";
+        _jwkJson = JsonSerializer.Serialize(jwk);
+    }
 
+    public ClientTokenManagementApiTests()
+    {
         var services = new ServiceCollection();
 
         services.AddDistributedMemoryCache();
@@ -28,7 +34,7 @@ public class ClientTokenManagementApiTests : IntegrationTestBase
                 client.ClientSecret = "secret";
                 client.Scope = "scope1";
                 client.HttpClient = IdentityServerHost.HttpClient;
-                client.DPoPJsonWebKey = JsonSerializer.Serialize(jwk);
+                client.DPoPJsonWebKey = _jwkJson;
             });
         services.AddClientCredentialsHttpClient("test", "test");
 
@@ -37,15 +43,16 @@ public class ClientTokenManagementApiTests : IntegrationTestBase
         _tokenService = provider.GetRequiredService<IClientCredentialsTokenManagementService>();
     }
 
-    [Fact(Skip = "enable once IS with dpop support is updated")]
+    [Fact]
     public async Task for_dpop_clients_GetAccessTokenAsync_should_obtain_token_with_cnf()
     {
         var token = await _tokenService.GetAccessTokenAsync("test");
         
         token.IsError.ShouldBeFalse();
         token.DPoPJsonWebKey.ShouldNotBeNull();
-        var header = Base64UrlEncoder.Decode(token.AccessToken!.Split('.')[0]);
-        var headerValues = JsonSerializer.Deserialize<Dictionary<string, object>>(header);
-        headerValues!.ShouldContainKey("cnf");
+        token.AccessTokenType.ShouldBe("DPoP");
+        var payload = Base64UrlEncoder.Decode(token.AccessToken!.Split('.')[1]);
+        var values = JsonSerializer.Deserialize<Dictionary<string, object>>(payload);
+        values!.ShouldContainKey("cnf");
     }
 }
